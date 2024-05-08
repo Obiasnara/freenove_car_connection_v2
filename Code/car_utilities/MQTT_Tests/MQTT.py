4903bc0fcb68e9ce473aba3c3f5d10516bf94e7e
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-05-08 15:50:58 +0100

    Added file header importing mqtt from phaos

--- Code/car_utilities/MQTT_Tests/MQTT.py
+++ Code/car_utilities/MQTT_Tests/MQTT.py
@@ -1,3 +1,23 @@
+import time
+import paho.mqtt.client as mqtt
+import json
+def on_publish(client, userdata, mid, reason_code, properties):
+    # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
+    try:
+        userdata.remove(mid)
+    except KeyError:
+        print("on_publish() is called with a mid not present in unacked_publish")
+        print("This is due to an unavoidable race-condition:")
+        print("* publish() return the mid of the message sent.")
+        print("* mid from publish() is added to unacked_publish by the main thread")
+        print("* on_publish() is called by the loop_start thread")
+        print("While unlikely (because on_publish() will be called after a network round-trip),")
+        print(" this is a race-condition that COULD happen")
+        print("")
+        print("The best solution to avoid race-condition is using the msg_info from publish()")
+        print("We could also try using a list of acknowledged mid rather than removing from pending list,")
+        print("but remember that mid could be re-used !")
+
 unacked_publish = set()
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_publish = on_publish
