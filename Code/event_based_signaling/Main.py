6bd619d350cb9f3e57cb3ec935c1d471eb37c55c
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-05-10 13:39:25 +0100

    Fix to motor

--- Code/event_based_signaling/Main.py
+++ Code/event_based_signaling/Main.py
@@ -3,15 +3,16 @@ import InitPythonPath
 from MQTT_Handler import MQTTHandler
 from MQTT_Motor_Module import Motor
 
+MQTT_BROKER_ADDRESS = "157.245.38.231"
+mqtt_handler = MQTTHandler(MQTT_BROKER_ADDRESS, client_id="car_motor_module")
+motor = Motor(mqtt_handler)
+
 def loop():
-    MQTT_BROKER_ADDRESS = "157.245.38.231"
-    mqtt_handler = MQTTHandler(MQTT_BROKER_ADDRESS, client_id="car_motor_module")
-    motor = Motor(mqtt_handler)
     while True:
         pass
 
 def destroy():
-    mqtt_handler.stop()
+    motor.mqtt_handler.stop()
 
 if __name__ == '__main__':
     try:
