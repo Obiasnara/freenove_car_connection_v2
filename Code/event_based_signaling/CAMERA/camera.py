801f3d93a5a0c0aaa7c530a7bede615fde382c21
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-05-14 15:04:51 +0100

    tests

--- Code/event_based_signaling/CAMERA/camera.py
+++ Code/event_based_signaling/CAMERA/camera.py
@@ -1,4 +1,11 @@
+import js2py
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
 picam2.start_and_capture_file("image.jpg")
+
+# Transform the main.js file into a Python function
+main = js2py.eval_js_file("main.js")
+
+# Call the start() function from the main.py file
+main.start()
