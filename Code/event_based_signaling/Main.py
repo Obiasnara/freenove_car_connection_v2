5776f7f98ff7eca9794123569952de7c167b992e
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-06-26 16:06:35 +0100

    azeazea

--- Code/event_based_signaling/Main.py
+++ Code/event_based_signaling/Main.py
@@ -5,6 +5,7 @@ from ENGINE.MQTT_Motor_Module import Motor
 from BATTERY.MQTT_Battery_Module import Battery
 from ULTRASONIC.MQTT_Ultrasonic_Module import Ultrasonic
 from CPU.MQTT_CPU_Module import CPU
+from CAMERA.MQTT_Camera_Module import Camera
 
 MQTT_BROKER_ADDRESS = "157.245.38.231"
 mqtt_handler = MQTTHandler(MQTT_BROKER_ADDRESS, client_id="car_motor_module")
@@ -12,6 +13,7 @@ motor = Motor(mqtt_handler)
 battery = Battery(mqtt_handler)
 ultrasonic = Ultrasonic(mqtt_handler)
 cpu = CPU(mqtt_handler)
+camera = Camera(mqtt_handler)
 
 def loop():
     while True:
@@ -22,6 +24,8 @@ def destroy():
     battery.destroy()
     ultrasonic.destroy()
     cpu.destroy()
+    camera.destroy()
+    mqtt_handler.stop()
 
 if __name__ == '__main__':
     try:
