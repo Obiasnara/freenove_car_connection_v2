938cf3740ddb13bc3e0dbd52f38b7582ab46582b
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-06-26 12:37:16 +0100

    qs

--- Code/event_based_signaling/ULTRASONIC/MQTT_Ultrasonic_Module.py
+++ Code/event_based_signaling/ULTRASONIC/MQTT_Ultrasonic_Module.py
@@ -18,6 +18,7 @@ class Ultrasonic(MQTT_Module_Interface):
         self.comm_handler = comm_handler
         self.sender = "Submodel1_Operation4"
         self.distance_temp = 0
+        print("Ultrasonic Module is ready!")
         self.getMessage()
 
     def getMessage(self):
