be114e0f8f9d906425606745df99aee080abf30a
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-06-27 10:56:54 +0100

    azd

--- Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
+++ Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
@@ -7,34 +7,15 @@ from Interfaces.MQTT_Module_Interface import MQTT_Module_Interface
 rtmp_url = "rtmp://157.245.38.231/live/stream1"
 class Camera(MQTT_Module_Interface):
     def __init__(self, comm_handler): 
+        self.comm_handler = comm_handler
+        self.sender = "Submodel1_Operation6"
+        self.getMessages()
+
         self.camera = Picamera2()
         self.video_config = self.camera.create_video_configuration()
         self.camera.configure(self.video_config)
-        ffmpeg_cmd = [
-            'ffmpeg',  # FFmpeg executable
-            '-hide_banner',  # Hide FFmpeg banner
-            '-loglevel', 'error',  # Suppress non-error FFmpeg logs
-            '-f', 'rawvideo',  # Input format: raw video
-            '-pix_fmt', 'bgr24',  # Pixel format: 24-bit BGR
-            '-r', '30',  # Input frame rate: 60 frames per second
-            '-i', '-',  # Read input from stdin
-            '-c:v', 'libx264',  # Video codec: H.264 with libx264 encoder
-            '-pix_fmt', 'yuv420p',  # Pixel format for output: YUV420p
-            '-preset', 'ultrafast',  # Encoding preset: ultrafast for speed
-            '-tune', 'zerolatency',  # Tune settings for low-latency streaming
-            '-movflags', '+faststart',  # Enable fast start for video playback
-            '-crf', '20',  # Constant Rate Factor for video quality
-            '-g', '1',  # Keyframe interval
-            '-b:v', '5M',  # Target video bitrate: 1 Mbps
-            '-f', 'flv',  # Output format: FLV (Flash Video)
-            '-an',  # Disable audio encoding
-            '-c:a', 'aac',  # Audio codec: AAC
-            '-bufsize', '1M',  # Buffer size for video encoding
-            '-maxrate', '5M',  # Maximum video bitrate: 1 Mbps
-            rtmp_url
-        ]
-        self.output = FfmpegOutput(ffmpeg_cmd)
-        #self.output = FfmpegOutput(f"-f flv {rtmp_url}")  
+        
+        self.output = FfmpegOutput(f"-f flv {rtmp_url}")  
         self.encoder = H264Encoder()
         self.start_streaming()
 
@@ -45,13 +26,15 @@ class Camera(MQTT_Module_Interface):
         self.camera.stop_recording()
 
     def on_message(self, client, userdata, message):
-        if message.topic == self.mqtt_topic:
-            self.camera.take_picture()
-            self.camera.send_picture()
-            self.camera.delete_picture()
+        pass
 
     def getMessages(self):
-        pass
+        def message_loop():  # This function will run in its own thread
+            while True:
+                self.comm_handler.publish(self.sender, rtmp_url)
+                time.sleep(30)  # Sleep within this thread only
+        thread = threading.Thread(target=message_loop)
+        thread.start()  # Start the thread
     
     def destroy(self):
-        pass
+        self.camera.stop_recording()
