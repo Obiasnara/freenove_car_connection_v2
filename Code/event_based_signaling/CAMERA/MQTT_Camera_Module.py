16e869d53b2ac601d0f1a49dbf1baf8231be2ed6
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-06-27 10:41:48 +0100

    zad

--- Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
+++ Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
@@ -2,22 +2,33 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration()
-picam2.configure(video_config)
+from Interfaces.MQTT_Module_Interface import MQTT_Module_Interface
 
-# Replace 'your_rtmp_url' with your actual RTMP server URL
 rtmp_url = "rtmp://157.245.38.231/live/stream1"
+class Camera(MQTT_Module_Interface):
+    def __init__(self, comm_handler): 
+        self.camera = Picamera2()
+        self.video_config = self.camera.create_video_configuration()
+        self.camera.configure(self.video_config)
 
-# Configure FFmpeg output for RTMP streaming
-output = FfmpegOutput(f"-f flv {rtmp_url}")  
+        self.output = FfmpegOutput(f"-f flv {rtmp_url}")  
+        self.encoder = H264Encoder()
+        self.start_streaming()
 
-encoder = H264Encoder()
-picam2.start_recording(encoder, output)
+    def start_streaming(self):
+        self.picam2.start_recording(self.encoder, self.output) 
 
-# Keep the stream running (you'll need to handle stopping in your application)
-try:
-    while True:
+    def stop_streaming(self):
+        self.picam2.stop_recording()
+
+    def on_message(self, client, userdata, message):
+        if message.topic == self.mqtt_topic:
+            self.camera.take_picture()
+            self.camera.send_picture()
+            self.camera.delete_picture()
+
+    def getMessages(self):
         pass
-except KeyboardInterrupt:
-    picam2.stop_recording()
+    
+    def destroy(self):
+        pass
