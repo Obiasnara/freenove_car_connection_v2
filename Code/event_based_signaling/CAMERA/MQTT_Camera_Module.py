3c08d09da1b2e85d795f169585a5bc1f5d61d29f
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-06-27 10:04:28 +0100

    encoder output

--- Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
+++ Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
@@ -42,7 +42,8 @@ class Camera(MQTT_Module_Interface):
         stream_name = "stream1"
         RTMP_SERVER_IP = "157.245.38.231"
         self.ffmpeg_output = FfmpegOutput(output_filename='-f rtmp://{}/live/{}'.format(RTMP_SERVER_IP, stream_name))
-        self.camera.start_recording(self.encoder, self.ffmpeg_output, quality=Quality.VERY_LOW)
+        self.encoder.add_output(self.ffmpeg_output)
+        self.camera.start_recording(self.encoder, "test.h264", quality=Quality.VERY_LOW)
         time.sleep(30)
         self.camera.stop_recording()
         self.camera.close()
