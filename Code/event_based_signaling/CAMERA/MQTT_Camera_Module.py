432395c692fddd2196a522634e28a9608cefd9b0
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-06-27 10:29:43 +0100

    azd

--- Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
+++ Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
@@ -10,35 +10,20 @@ RTMP_SERVER_IP = "157.245.38.231"
 STREAM_NAME = "stream1"
 
 class Camera(MQTT_Module_Interface):
-    def __init__(self, comm_handler):  # Optional comm_handler for future use
+    def __init__(self, comm_handler=None): 
         self.picam2 = Picamera2()
-        self.picam2.configure(self.picam2.create_video_configuration(main={"size": (640, 480)}))  # Set desired resolution
+        video_config = self.picam2.create_video_configuration(main={"size": (640, 480)})
+        self.picam2.configure(video_config) 
         self.encoder = H264Encoder(bitrate=1000000, repeat=True)  
-        self.ffmpeg_output = FfmpegOutput('rtmp://{}/live/{}'.format(RTMP_SERVER_IP, STREAM_NAME))  
+        self.output = FfmpegOutput(f'rtmp://{RTMP_SERVER_IP}/live/{STREAM_NAME}')
+
         self.streaming_thread = None 
-        self.start_streaming() 
 
     def start_streaming(self):
-        ffmpeg_cmd = [
-            'ffmpeg',
-            '-hide_banner', '-loglevel', 'error',
-            '-f', 'h264',  # Input format is H.264 
-            '-i', '-',     # Input from stdin (pipe)
-            '-c:v', 'copy',  # No re-encoding for speed (if needed you can add -c:v libx264)
-            '-f', 'flv',  # Output format is FLV (required for RTMP)
-            'rtmp://{}/live/{}'.format(RTMP_SERVER_IP, STREAM_NAME)
-        ]
-
-        self.ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
-        self.picam2.start_recording(self.encoder, self.ffmpeg_process.stdin)  
+        self.picam2.start_recording(self.encoder, self.output) 
 
     def stop_streaming(self):
         self.picam2.stop_recording()
-        if self.ffmpeg_process:
-            self.ffmpeg_process.stdin.close()
-            self.ffmpeg_process.wait() 
-        if self.streaming_thread:
-            self.streaming_thread.join() 
 
     def on_message(self, client, userdata, message):
         if message.topic == self.mqtt_topic:
