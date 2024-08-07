2b2953898302ac9eedbda981c5ea2a9cd4025d6e
BRAVE-COEUR Obiasnara <picaudnico@gmail.com>
2024-08-07 13:38:17 +0100

    fgyhui

--- Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
+++ Code/event_based_signaling/CAMERA/MQTT_Camera_Module.py
@@ -30,7 +30,6 @@ class Camera(MQTT_Module_Interface):
         self.getMessages()
 
         self.output = FfmpegOutput(f"-f flv {rtmp_url}")  
-        #self.output2 = FfmpegOutput(f"-f mpegts udp://138.250.145.156:5000 -preset ultrafast -tune zerolatency -x264-params keyint=15:scenecut=0 -fflags nobuffer -probesize 32 -payload_type 96")
         self.imageSender = None
         self.encoder = H264Encoder()
         self.encoder.output = self.output
@@ -41,7 +40,6 @@ class Camera(MQTT_Module_Interface):
             while self.stream:
                 if self.imageSender is not None:
                     image = self.camera.capture_array()
-                    print(image)
                     self.imageSender.send_image(self.hostName, image)
                 else:
                     print("ImageSender is None")
